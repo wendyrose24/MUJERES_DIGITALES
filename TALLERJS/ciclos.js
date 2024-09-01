f4f6ffce52fc1aea4cb450b626a9f99a38bec8f2
ADSO <wendyrosero48@gmail.com>
2024-09-01 17:56:09 -0500

    agrego TALLER DE JAVASCRIPT
    Agregue los ejercicios calificables de javascript

---
+++ TALLERJS/ciclos.js
@@ -0,0 +1,5 @@
+
+// Ciclos
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
---
+++ TALLERJS/variablesycond.js
@@ -0,0 +1,8 @@
+//Variables & Condicionales
+let temperatura = 32; 
+
+if (temperatura > 30) {
+    console.log("Hace calor");
+} else {
+    console.log("Hace frío");
+}
